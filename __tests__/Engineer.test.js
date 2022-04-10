a26f90231047f3d57e46bdd5e9187c11e5256c2e
The Creator <barnesweston@gmail.com>
2022-04-10 17:48:52 -0500

    pass 2 of 4 engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -6,4 +6,22 @@ test('engineer has employee properties', () => {
     expect(engineer.name).toBe('Adam', 'adam@email.com');
     expect(engineer.id).toEqual(1);
     expect(engineer.email).toBe('adam@email.com');
+});
+
+test('engineer has a github value', () => {
+    const engineer = new Engineer('Adam', 'email', 'adamcoder')
+
+    expect(engineer.github).toBe('adamcoder');
+});
+
+test('get github value', () => {
+    const engineer = new Engineer('Adam', 'email', 'adamcoder');
+
+    expect(getGithub).toBe('adamcoder');
+});
+
+test('getRole overrides to Engineer', () => {
+    const engineer = new Engineer('Adam');
+
+    expect(getRole).toBe('Engineer');
 })
