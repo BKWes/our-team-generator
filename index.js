7524f3d94d9d035ea130ad5e35d96d77cf2e669d
The Creator <barnesweston@gmail.com>
2022-04-15 14:36:03 -0500

    add inquirer prompts for engineer and intern; bug: stuck in engineer prompt loop

--- index.js
+++ index.js
@@ -1,5 +1,99 @@
 const inquirer = require('inquirer');
 
+function engineerPrompt() {
+    inquirer
+      .prompt([
+        //   engineer name, id, github
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: "What is this engineer's name?"
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "Enter this engineer's github name"
+        },
+        {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Would you like to add another employee?'
+        },
+        {
+            type: 'checkbox',
+            name: 'employeeType',
+            message: 'Add an Engineer or Intern?',
+            choices: ['Engineer', 'Intern'],
+            when: ({ addEmployee }) => {
+                if (addEmployee) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+      ])
+      .then((engineerAnswers) => {
+        console.log(engineerAnswers);
+        if ({ employeeType: ['Engineer']} ) {
+            engineerPrompt()
+        } else if ({ employeeType: ['Intern'] }) {
+            internPrompt()
+        } else {
+            // finish prompt and generate HTML with given
+        }
+    });
+};
+
+function internPrompt() {
+    inquirer
+      .prompt([
+        //   intern name, ID, email, school
+        {
+            type: 'input',
+            name: 'internName',
+            message: "What is this intern's name?"
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "Enter this intern's email address"
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "Enter this intern's school"
+        },
+        {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Would you like to add another employee?'
+        },
+        {
+            type: 'checkbox',
+            name: 'employeeType',
+            message: 'Add an Engineer or Intern?',
+            choices: ['Engineer', 'Intern'],
+            when: ({ addEmployee }) => {
+                if (addEmployee) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+      ])
+      .then((internAnswers) => {
+        console.log(internAnswers);
+        if ({ employeeType: ['Engineer']} ) {
+            engineerPrompt()
+        } else if ({ employeeType: ['Intern'] }) {
+            internPrompt()
+        } else {
+            // finish prompt and generate HTML with given
+        }
+    });
+}
 
 inquirer
   .prompt([
@@ -46,6 +140,13 @@ inquirer
   .then((answers) => {
     // Use user feedback for... whatever!!
     console.log(answers);
+    if ({ employeeType: ['Engineer']} ) {
+        engineerPrompt()
+    } else if ({ employeeType: ['Intern'] }) {
+        internPrompt()
+    } else {
+        // finish prompt and generate HTML with given
+    }
   })
   .catch((error) => {
     if (error.isTtyError) {
