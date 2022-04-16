32daf7db875714de3a711e24ea28798b7b71ef85
The Creator <barnesweston@gmail.com>
2022-04-16 18:02:44 -0400

    add intern extension from employee; pass 2 of 4 tests (still stuck on adding method to extended obj)

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,8 +1,27 @@
 const Intern = require('../lib/Intern');
 
-test('create new intern object', () => {
-    const intern = new Intern('Ally');
+test('intern has employee properties', () => {
+    const intern = new Intern('Ally', 'ally@email.com');
 
-    expect(intern.name).toBe('Ally');
+    expect(intern.name).toBe('Ally', 'ally@email.com');
+    expect(intern.id).toEqual(1);
+    expect(intern.email).toBe('ally@email.com');
 });
 
+test('intern has school property', () => {
+    const intern = new Intern('Ally', 'ally@email.com', 'CSU');
+
+    expect(intern.school).toBe('CSU');
+});
+
+test('get school value', () => {
+    const intern = new Intern('Ally', 'email', 'CSU');
+
+    expect(getSchool()).toBe('CSU');
+});
+
+test('getRole overrides to Intern', () => {
+    const intern = new Intern('Ally');
+
+    expect(getRole()).toBe('Intern');
+});
