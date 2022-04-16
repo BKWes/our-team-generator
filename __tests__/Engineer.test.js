174a06282408853795da154bff99c56bb609be39
The Creator <barnesweston@gmail.com>
2022-04-16 17:45:00 -0400

    try different formats, still unsuccessful on last two tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -17,11 +17,11 @@ test('engineer has a github value', () => {
 test('get github value', () => {
     const engineer = new Engineer('Adam', 'email', 'adamcoder');
 
-    expect(getGithub).toBe('adamcoder');
+    expect(getGithub()).toBe('adamcoder');
 });
 
 test('getRole overrides to Engineer', () => {
     const engineer = new Engineer('Adam');
 
-    expect(getRole).toBe('Engineer');
+    expect(getRole()).toBe('Engineer');
 })
