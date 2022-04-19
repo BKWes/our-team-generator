fd3dd2f480cf0704e93716e5384ed8286bbcf1b4
The Creator <barnesweston@gmail.com>
2022-04-19 16:10:43 -0500

    rewrite prompt to have one prompt not multiple being called in functions

--- index.js
+++ index.js
@@ -5,170 +5,86 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-function managerPrompt() {
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-        type: 'input',
-        name: 'managerName',
-        message: "What is the team manager's name"
-    },
-    {
-        type: 'number',
-        name: 'managerId',
-        message: "Enter the manager's ID",
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: "Enter the manager's email"
-    },
-    {
-        type: 'input',
-        name: 'managerNum',
-        message: "Enter the manager's office number"
-    },
-    {
-        type: 'confirm',
-        name: 'addEmployee',
-        message: "Would you like to add employees to your team?"
-    },
-    {
-        type: 'checkbox',
-        name: 'employeeType',
-        message: 'Add an Engineer or Intern?',
-        choices: ['Engineer', 'Intern'],
-        when: ({ addEmployee }) => {
-            if (addEmployee) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
-  ])
-  .then((managerData) => {
-      console.log(managerData);
-    if (!managerData.addEmployee) {
-          console.log('prompt end');
-          return managerData;
+// rewrite inquirer prompt
+const userPrompt = employeeData => {
+    if (!employeeData) {
+        employeeData = [];
     }
-    if (managerData.employeeType == 'Engineer' ) {
-        console.log('adding engineer');
-        engineerPrompt()
-    } else if (managerData.employeeType == 'Intern') {
-        console.log('adding intern');
-        internPrompt()
-    }
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
-}
+// pass in employee data and create an empty array if it does not already exist
 
-function engineerPrompt() {
-    inquirer
-      .prompt([
-        //   engineer name, id, github
+// first ask employee job title, then name, and email
+    return inquirer.prompt([
         {
-            type: 'input',
-            name: 'engineerName',
-            message: "What is this engineer's name?"
+            type: 'list',
+            name: 'title',
+            message: "What is this employee's job title?",
+            choices: ['Manager', 'Engineer', 'Intern']
         },
         {
             type: 'input',
-            name: 'engineerGithub',
-            message: "Enter this engineer's github name"
-        },
-        {
-            type: 'confirm',
-            name: 'addEmployee',
-            message: "Would you like to add another employee to your team?"
-        },
-        {
-            type: 'checkbox',
-            name: 'employeeType',
-            message: 'Add an Engineer or Intern?',
-            choices: ['Engineer', 'Intern'],
-            when: ({ addEmployee }) => {
-                if (addEmployee) {
+            name: 'name',
+            message: "What is this employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
                     return true;
                 } else {
+                    console.log("You must enter this employee's name.");
                     return false;
                 }
             }
-        }
-      ]).then((engineerData) => {
-        console.log(engineerData);
-        if (!engineerData.addEmployee){
-            console.log('end prompt');
-            return;
-        }
-
-        if (engineerData.employeeType == 'Engineer') {
-            engineerPrompt()
-        } else if (engineerData.employeeType == 'Intern') {
-            internPrompt()
-        }
-      }) 
-};
-
-function internPrompt() {
-    inquirer
-      .prompt([
-        //   intern name, ID, email, school
-        {
-            type: 'input',
-            name: 'internName',
-            message: "What is this intern's name?"
         },
         {
             type: 'input',
-            name: 'internEmail',
-            message: "Enter this intern's email address"
-        },
-        {
-            type: 'input',
-            name: 'internSchool',
-            message: "Enter this intern's school"
-        },
-        {
-            type: 'confirm',
-            name: 'addEmployee',
-            message: "Would you like to add another employee to your team?"
-        },
-        {
-            type: 'checkbox',
-            name: 'employeeType',
-            message: 'Add an Engineer or Intern?',
-            choices: ['Engineer', 'Intern'],
-            when: ({ addEmployee }) => {
-                if (addEmployee) {
+            name: 'email',
+            message: "What is this employee's email address?",
+            validate: emailInput => {
+                if (emailInput) {
                     return true;
                 } else {
+                    console.log("You must provide an email");
                     return false;
                 }
             }
+        },
+        // if engineer ask github
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is this engineer's github?",
+            when: (answers) => answers.job === 'Engineer'
+        },
+        // if intern ask school 
+        {
+            type: 'input',
+            name: 'school',
+            message: "Where does this intern attend school?",
+            when: (answers) => answers.job === 'Intern'
+        },
+        // if manager ask office number
+        {
+            type: 'input',
+            name: 'office',
+            message: "What is this manager's office number?",
+            when: (answers) => answers.job === 'Manager'
+        },
+        // ask for another employee?
+        {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Would you like to add another employee?',
+            default: false
         }
-      ]).then((internData) => {
-        console.log(internData);
-        if (!internData.addEmployee) {
-            console.log('end of prompt');
-            return;
-        }
-
-        if (internData.employeeType =- 'Engineer') {
-            engineerPrompt()
-        } else if (internData.employeeType =- 'Intern') {
-            internPrompt()
+    ])
+    .then(employeeResults => {
+        employeeData.push(employeeResults)
+        if (employeeResults.addEmployee) {
+            return userPrompt(employeeData);
+        } else {
+            return employeeData;
         }
     })
-};
+}
+// push results to employee data array in promise - if add another employee return function; else- return data
 
-managerPrompt()
-//   .then(generatePage(mangaerData));
+
+userPrompt()
