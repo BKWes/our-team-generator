c8c0c7757d16200864e7eb3be16e9bd831a8a4f5
Weston <barnesweston@gmail.com>
2022-04-18 19:06:01 -0500

    add variable declarations/dependencies

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 const generatePage = require('./src/html-template');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
 function managerPrompt() {
 inquirer
